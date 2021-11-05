ba7fac9fc2baacc2702239c9ad3086214e6805b9
Peter Liedholm <peter.liedholm@securemeters.com>
2021-11-05 11:16:00 +0100

    Documented tests and results

--- WeakOrStrong/IAR/main.c
+++ WeakOrStrong/IAR/main.c
@@ -2,6 +2,17 @@
 
 extern char* foo(void);
 
+// Tested combinations of foo implementation by exclude in project
+// Exe    Lib1            Lib2 Result
+// =================================
+// strong -               -      OK, strong
+// -      weak            -      OK, weak
+// strong weak            -      OK, strong
+// -      strong          -      OK, strong
+// -      weak and strong -      OK, strong
+// -      weak            strong OK, strong
+// strong weak            strong OK?, strong (main)
+
 int main()
 {
   puts(foo());
